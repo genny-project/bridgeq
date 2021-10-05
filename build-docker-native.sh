3fbfe2e4da84f8c5978c0da43e0a376a27c258ca
Adam Crow <adamcrow63@gmail.com>
2021-10-05 22:53:30 +1100

    Got native build and native docker running

--- build-docker-native.sh
+++ build-docker-native.sh
@@ -18,9 +18,12 @@ version=$version-native
 USER=`whoami`
 #./mvnw package -Pnative -Dquarkus.native.container-build=true -DskipTests=false
 #./mvnw package -Pnative -Dquarkus.native.container-build=true
-# MUST HAVE GRAALVM installed!!!
-./mvnw package -Pnative -Dquarkus.container-image.build=true
+# MUST HAVE GRAALVM installed!!! (so run under linux with lots of memory)
+#./mvnw package -Pnative  -Dquarkus.native.container-build=true  -Dquarkus.container-image.build=true
 
-docker build -f src/main/docker/Dockerfile.native -t ${USER}/${project}:${version} .
+#th lots of memory)Build using Linux on an osx
+./mvnw package -Pnative  -Dquarkus.native.container-build=true  -Dquarkus.container-image.build=true
+
+#docker build -f src/main/docker/Dockerfile.native -t ${USER}/${project}:${version} .
 docker tag ${USER}/${project}:${version} ${org}/${project}:${version}
 docker tag ${USER}/${project}:${version} ${org}/${project}:native
